4feea790583675ab93790eb66c6be6a56ed50aca
Jordan Saxe <jordansaxe15@gmail.com>
2013-10-29 14:03:54 -0400

    Updated profile page and added scoped status updates to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Treebook::Application.routes.draw do
+  get "profiles/show"
   devise_for :users
 
   devise_scope :user do
@@ -10,6 +11,8 @@ Treebook::Application.routes.draw do
   resources :statuses
     get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
+
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
